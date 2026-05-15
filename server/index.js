97e1e648af341cd2d42013f1acec28d28d15712d
Sherzod Artikbayev <info@web-pro.uz>
2026-05-15 21:35:21 +0500

    feat: Implented Socket.io

---
+++ server/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+
+const express = require('express')
+const http = require('http')
+
+const app = express()
+const server = http.createServer(app)
+
+const { Server } = require('socket.io')
+
+const io = new Server(server, {
+	cors: process.env.CLIENT_URL,
+})
+
+io.on('connection', socket => {
+	socket.on('send_message', message => {
+		socket.broadcast.emit('receive_message', message)
+	})
+
+	socket.on('user_typing', data => {
+		socket.broadcast.emit('user_typing', data)
+	})
+
+	socket.on('new_user', data => {
+		socket.broadcast.emit('new_user', data)
+	})
+})
+
+const PORT = process.env.PORT || 4000
+server.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
